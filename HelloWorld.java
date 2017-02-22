95c950739ea8da3cb0636a43ef474070c217eb42
Gallouche <gallandat.t@gmail.com>
2017-02-22 09:03:02 +0100

    Ajout classe Pouet

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,4 +3,8 @@ public class HelloWorld {
 	public static void main (String[] args){
 		System.out.println("HelloWorld !");
 	}
+
+	public void Pouet(){
+		System.out.println("Pouet ! Pouet !");
+	}
 }
